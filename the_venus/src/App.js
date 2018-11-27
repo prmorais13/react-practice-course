cdb66c2fd90bd6208d53b748a76d9273128c6199
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-11-26 21:16:39 -0300

    Criando cabecalho

--- the_venus/src/App.js
+++ the_venus/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
+import './resources/styles.css';
+import Header from './components/header_footer/Header';
+
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
