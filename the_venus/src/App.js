bc5acdead94408a17c93d55a9eb9bb0d082bdf95
prmorais13 <prmorais_13@hotmail.com>
2018-11-27 16:08:04 -0300

    Componente Venue NFO

--- the_venus/src/App.js
+++ the_venus/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenueNfo from './components/venueNfo';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
       >
         <Header />
         <Featured />
+        <VenueNfo />
       </div>
     );
   }
