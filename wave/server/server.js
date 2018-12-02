2e09bb8267f8e37f55d88327538a8a7ecb306c6e
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-12-01 22:09:54 -0300

    SERVER: User model

--- wave/server/server.js
+++ wave/server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
-const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
+
+const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE);
@@ -12,6 +12,15 @@ mongoose.connect(process.env.DATABASE);
 app.use(bodyParser.urlencoded({ extends: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+//        MODELS
+const { User } = require('./models/user');
+
+//        USERS
+
+app.post('/api/users/register', (req, res) => {
+  res.status(200);
+});
 
 const port = process.env.PORT || 3002;
 
