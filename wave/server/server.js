c1a4044b787ffac50614b86343688d70d699b189
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-12-04 20:39:48 -0300

    SERVER: Brand model and routes

--- wave/server/server.js
+++ wave/server/server.js
@@ -15,12 +15,35 @@ app.use(cookieParser());
 
 //========= MODELS =========
 const { User } = require('./models/user');
+const { Brand } = require('./models/brand');
 
 //======== MIDDLEWARES ===========
 const { auth } = require('./middlewares/auth');
+const { admin } = require('./middlewares/admin');
+
+//========= BRAND ==========
+app.post('/api/product/brand', auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
+
+  brand.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+
+    res.status(200).json({
+      success: true,
+      brand: doc
+    });
+  });
+});
+
+app.get('/api/product/brands', (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if (err) return res.status(400).send(err);
+
+    res.status(200).send(brands);
+  });
+});
 
 //========= USERS ==========
-
 app.get('/api/users/auth', auth, (req, res) => {
   res.status(200).json({
     isAdmin: req.user.role === 0 ? false : true,
