c62a159fa8bfd93090c70160777d45034b4faed6
prmorais13 <prmorais_13@hotmail.com>
2018-12-05 14:35:46 -0300

    SERVER: Getting product by order

--- wave/server/server.js
+++ wave/server/server.js
@@ -26,6 +26,23 @@ const { admin } = require('./middlewares/admin');
 //================================
 //            PRODUCTS
 //================================
+
+app.get('/api/product/articles', (req, res) => {
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+  Product.find()
+    .populate('brand')
+    .populate('wood')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, articles) => {
+      if (err) return res.status(400).send(err);
+      res.send(articles);
+    });
+});
+
 app.get('/api/product/article_by_id', (req, res) => {
   let type = req.query.type;
   let items = req.query.id;
