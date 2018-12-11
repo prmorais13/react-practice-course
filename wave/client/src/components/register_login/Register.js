4fa44457d65b82facc100bf4a1c34fa4839f0dd9
prmorais13 <prmorais_13@hotmail.com>
2018-12-11 16:13:10 -0300

    CLIENT: Creating the Register - Incompleto

---
+++ wave/client/src/components/register_login/Register.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import FormField from '../utils/form/FormField';
+import { update, generateData, isFormValid } from '../utils/form/FormActions';
+import { loginUser } from '../../actions/user_actions';
+
+class Register extends Component {
+  state = {
+    formError: false,
+    formSuccess: '',
+    formdata: {
+      name: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'name_input',
+          type: 'text',
+          placeholder: 'Entre com o nome'
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ''
+      },
+      lastname: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'lastname_input',
+          type: 'text',
+          placeholder: 'Entre com o sobrenome'
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ''
+      },
+      email: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'email_input',
+          type: 'email',
+          placeholder: 'Entre com o e-mail'
+        },
+        validation: {
+          required: true,
+          email: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ''
+      },
+      password: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'password_input',
+          type: 'password',
+          placeholder: 'Entre com a senha'
+        },
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ''
+      },
+      confirmPassword: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'confirm_password_input',
+          type: 'password',
+          placeholder: 'Confirme a senha'
+        },
+        validation: {
+          required: true,
+          confirm: 'password'
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ''
+      }
+    }
+  };
+
+  updateForm = () => {};
+
+  submitForm = event => {};
+
+  render() {
+    return (
+      <div className="page_wrapper">
+        <div className="container">
+          <div className="register_login_container">
+            <div className="left">
+              <form onSubmit={event => this.submitForm(event)}>
+                <h2>Informações Pessoais</h2>
+                <div className="form_block_two">
+                  <div className="block">
+                    <FormField
+                      id={'name'}
+                      formdata={this.state.formdata.name}
+                      change={element => this.updateForm(element)}
+                    />
+                  </div>
+                  <div className="block">
+                    <FormField
+                      id={'lastname'}
+                      formdata={this.state.formdata.lastname}
+                      change={element => this.updateForm(element)}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <FormField
+                    id={'email'}
+                    formdata={this.state.formdata.email}
+                    change={element => this.updateForm(element)}
+                  />
+                </div>
+                <h2>Verificação de senha</h2>
+                <div className="form_block_two">
+                  <div className="block">
+                    <FormField
+                      id={'password'}
+                      formdata={this.state.formdata.password}
+                      change={element => this.updateForm(element)}
+                    />
+                  </div>
+                  <div className="block">
+                    <FormField
+                      id={'confirmPassword'}
+                      formdata={this.state.formdata.confirmPassword}
+                      change={element => this.updateForm(element)}
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect()(Register);
--- wave/client/src/routes.js
+++ wave/client/src/routes.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import Layout from './hoc/Layout';
 import RegisterLogin from './components/register_login';
+import Register from './components/register_login/Register';
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/register" exact component={Register} />
         <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/" exact component={Home} />
       </Switch>
