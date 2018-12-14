d234b06b6a19f9be1d5d214360cdde344f803817
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-12-13 21:56:57 -0300

    CLIENT: Finishing Header links

--- wave/client/src/components/header_footer/header/index.js
+++ wave/client/src/components/header_footer/header/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import { connect } from 'react-redux';
+
+import { logoutUser } from '../../../actions/user_actions';
 
 class Header extends Component {
   state = {
@@ -41,11 +43,39 @@ class Header extends Component {
     ]
   };
 
-  defaultLink = (item, i) => (
-    <Link to={item.linkTo} key={item.i}>
-      {item.name}
-    </Link>
-  );
+  logoutHandler = () => {
+    this.props.dispatch(logoutUser()).then(response => {
+      if (response.payload.success) {
+        this.props.history.push('/');
+      }
+    });
+  };
+
+  defaultLink = (item, i) =>
+    item.name === 'Logout' ? (
+      <div
+        className="log_out_link"
+        key={i}
+        onClick={() => this.logoutHandler()}
+      >
+        {item.name}
+      </div>
+    ) : (
+      <Link to={item.linkTo} key={i}>
+        {item.name}
+      </Link>
+    );
+
+  cartLink = (item, i) => {
+    const user = this.props.user.userData;
+
+    return (
+      <div className="cart_link" key={i}>
+        <span>{user.cart ? user.cart.length : 0}</span>
+        <Link to={item.linkTo}>{item.name}</Link>
+      </div>
+    );
+  };
 
   showLinks = type => {
     let list = [];
@@ -65,7 +95,11 @@ class Header extends Component {
     }
 
     return list.map((item, i) => {
-      return this.defaultLink(item, i);
+      if (item.name !== 'Meu Carrinho') {
+        return this.defaultLink(item, i);
+      } else {
+        return this.cartLink(item, i);
+      }
     });
   };
 
@@ -92,4 +126,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(withRouter(Header));
