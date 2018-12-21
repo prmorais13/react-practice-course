49c648aa029eea0d3be4c6add32f7cc52f78d7e0
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-12-20 21:23:00 -0300

    CLIENT: Finishing checkboxes

--- wave/client/src/components/utils/CollapseCheckbox.js
+++ wave/client/src/components/utils/CollapseCheckbox.js
@@ -1,8 +1,104 @@
 import React, { Component } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleUp, faAngleDown } from '@fortawesome/fontawesome-free-solid';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Checkbox from '@material-ui/core/Checkbox';
+import Collapse from '@material-ui/core/Collapse';
+
 class CollapseCheckbox extends Component {
+  state = {
+    open: false,
+    checked: []
+  };
+
+  componentDidMount() {
+    if (this.props.initState) {
+      this.setState({
+        open: this.props.initState
+      });
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
+  handleAngle = () =>
+    this.state.open ? (
+      <FontAwesomeIcon className="icon" icon={faAngleUp} />
+    ) : (
+      <FontAwesomeIcon className="icon" icon={faAngleDown} />
+    );
+
+  renderList = () =>
+    this.props.list
+      ? this.props.list.map(value => (
+          <div>
+            <ListItem key={value._id} style={{ padding: '10px 0' }}>
+              <ListItemText primary={value.name} />
+              <ListItemSecondaryAction>
+                <Checkbox
+                  color="primary"
+                  onChange={this.handleToggle(value._id)}
+                  checked={this.state.checked.indexOf(value._id) !== -1}
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+          </div>
+        ))
+      : null;
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState(
+      {
+        checked: newChecked
+      },
+      () => {
+        this.props.handleFilters(newChecked);
+      }
+    );
+  };
+
   render() {
-    return <div />;
+    return (
+      <div className="collapse_items_wrapper">
+        <List style={{ borderBottom: '1px solid #dbdbdb' }}>
+          <ListItem
+            onClick={this.handleClick}
+            style={{ padding: '10px 23px 10px 0' }}
+          >
+            <ListItemText
+              className="collapse_title"
+              primary={this.props.title}
+            />
+            {this.handleAngle()}
+          </ListItem>
+          <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              {this.renderList()}
+            </List>
+          </Collapse>
+        </List>
+      </div>
+    );
   }
 }
 
--- wave/client/src/components/utils/shop/index.js
+++ wave/client/src/components/utils/shop/index.js
@@ -3,21 +3,64 @@ import { connect } from 'react-redux';
 
 import PageTop from './PageTop';
 import { getBrands, getWoods } from '../../../actions/product_actions';
+import CollapseCheckbox from '../CollapseCheckbox';
+import { frets } from '../form/fixed_categories';
 
 class Shop extends Component {
+  state = {
+    grid: '',
+    limit: 6,
+    skip: 0,
+    filters: {
+      brand: [],
+      frets: [],
+      wood: [],
+      price: []
+    }
+  };
+
   componentDidMount() {
     this.props.dispatch(getBrands());
     this.props.dispatch(getWoods());
   }
 
+  handleFilters = (filters, category) => {
+    const newFilters = { ...this.state.filters };
+    newFilters[category] = filters;
+
+    this.setState({
+      filters: newFilters
+    });
+  };
+
   render() {
+    console.log(this.state.filters);
     const products = this.props.products;
     return (
       <div>
         <PageTop title="Browser Products" />
         <div className="container">
           <div className="shop_wrapper">
-            <div className="left">Left</div>
+            <div className="left">
+              <CollapseCheckbox
+                initState={true}
+                title="Brands"
+                list={products.brands}
+                handleFilters={filters => this.handleFilters(filters, 'brand')}
+              />
+              <CollapseCheckbox
+                initState={false}
+                title="Frets"
+                list={frets}
+                handleFilters={filters => this.handleFilters(filters, 'frets')}
+              />
+              <CollapseCheckbox
+                initState={true}
+                title="Wood"
+                list={products.woods}
+                handleFilters={filters => this.handleFilters(filters, 'wood')}
+              />
+            </div>
             <div className="right">Right</div>
           </div>
         </div>
